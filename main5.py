d4364fab8597aa98ec049e6cd591658c89166439
vath414 <dydyloy69@gmail.com>
2025-11-01 16:50:49 +0700

    push

---
+++ main5.py
@@ -0,0 +1,5 @@
+from common import setup,smooth_scroll,post
+if __name__ == "__main__":
+    driver=setup("cookie5")
+    smooth_scroll(driver)
+#18+Movies0768
---
+++ main6.py
@@ -0,0 +1,6 @@
+from common import setup,smooth_scroll,post
+if __name__ == "__main__":
+    driver=setup("cookie6")
+    smooth_scroll(driver)
+#MiaKhalifa6863
+
