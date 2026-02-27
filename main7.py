00ba3f56ab0b648e3061b8cd36bcce0a5a159396
vath414 <dydyloy69@gmail.com>
2026-02-27 23:19:46 +0700

    change some

--- main7.py
+++ main7.py
@@ -1,7 +1,7 @@
 from common import retweet_url, setup,smooth_scroll,post
 if __name__ == "__main__":
     driver=setup("cookie7")
-    retweet_url(driver, "https://x.com/username/status/123456789")
+    retweet_url(driver, "https://x.com/Dirtymom32/status/2027378892504477842")
     driver.save_screenshot("debug.png")
     driver.quit()
 #swapmom
