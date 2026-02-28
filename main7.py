10bdabb4186d4c978c14a3e9a540aef669d04c1a
vath414 <dydyloy69@gmail.com>
2026-02-28 10:46:05 +0700

    change some

--- main7.py
+++ main7.py
@@ -1,9 +1,9 @@
-from common import retweet_url, setup,smooth_scroll,post,GotoProfile
+from common import retweet_to_community, setup,smooth_scroll,post,GotoProfile
 if __name__ == "__main__":
     driver=setup("cookie7")
     smooth_scroll(driver)
     GotoProfile(driver)
-    retweet_url(driver,"Bokep Indo Viral")
+    retweet_to_community(driver, "Bokep Indo Viral")
     driver.save_screenshot("debug.png")
     driver.quit()
 #swapmom
