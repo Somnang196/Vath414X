6404bfbe2103301d34556763a2e11ff5c1500ccb
vath414 <dydyloy69@gmail.com>
2026-03-05 22:18:06 +0700

    add more

---
+++ main11.py
@@ -0,0 +1,8 @@
+from common import  setup,work
+import random, time
+
+if __name__ == "__main__":
+  # Only setup driver if selected
+    driver = setup("cookie11")
+    work(driver,"AvaAdam136653")
+#https://x.com/AvaAdam136653
