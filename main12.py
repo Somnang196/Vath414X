9111927396ea591ca060651137ca04861a7fc31b
vath414 <dydyloy69@gmail.com>
2026-05-19 19:00:06 +0700

    active session

--- main12.py
+++ main12.py
@@ -4,5 +4,6 @@ import random, time
 if __name__ == "__main__":
   # Only setup driver if selected
     driver = setup("cookie_IvanaWijoyo")
-    smooth_scroll(driver)
+    # smooth_scroll(driver)
+    work(driver,"IvanaWijoyo","https://x.com/Viviwie6c")
 #https://x.com/IvanaWijoyo
--- main13.py
+++ main13.py
@@ -4,5 +4,6 @@ import random, time
 if __name__ == "__main__":
   # Only setup driver if selected
     driver = setup("cookie_KevinSantoso095")
-    smooth_scroll(driver)
+    # smooth_scroll(driver)
+    work(driver,"KevinSantoso095","https://x.com/Viviwie6c")
 #https://x.com/KevinSantoso095
