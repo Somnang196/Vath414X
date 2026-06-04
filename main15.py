3aa8313705fc9bc4a3a8331c9cf5db7920f5c3d6
vath414 <dydyloy69@gmail.com>
2026-06-04 22:00:50 +0700

    delete 2 accounts

--- main15.py
+++ main15.py
@@ -3,7 +3,8 @@ import random, time
 
 if __name__ == "__main__":
   # Only setup driver if selected
-    driver = setup("cookie_ScorchedX")
-    # smooth_scroll(driver)
-    work(driver,"ScorchedX8247","https://x.com/Viviwie6c")
+    # driver = setup("cookie_ScorchedX")
+    # # smooth_scroll(driver)
+    # work(driver,"ScorchedX8247","https://x.com/Viviwie6c")
+  print("it is suspend")
 #https://x.com/ScorchedX8247
--- main16.py
+++ main16.py
@@ -3,7 +3,8 @@ import random, time
 
 if __name__ == "__main__":
   # Only setup driver if selected
-    driver = setup("cookie_SelectiveX")
-    # smooth_scroll(driver)
-    work(driver,"SelectiveX4574","https://x.com/Viviwie6c")
+    # driver = setup("cookie_SelectiveX")
+    # # smooth_scroll(driver)
+    # work(driver,"SelectiveX4574","https://x.com/Viviwie6c")
+    print("it is suspend")
 #https://x.com/SelectiveX4574
